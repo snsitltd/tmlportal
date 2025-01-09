cee71e632780fc7b77f82921d80be768b203ada3
Devam Joshi <developer@snsitltd.com>
2025-01-09 13:34:14 +0000

    Change URL For UAT Testing

--- application/config/config.php
+++ application/config/config.php
@@ -29,11 +29,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // $base  = $protocol."://".$_SERVER['HTTP_HOST'];
 // $base .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 // $config['base_url'] = $base;
-$protocol = isset($_SERVER["HTTPS"]) ? 'https' : 'http';
-$base  = $protocol."://".$_SERVER['HTTP_HOST'];
-$base .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
-$config['base_url'] = $base;
+// $protocol = isset($_SERVER["HTTPS"]) ? 'https' : 'http';
+// $base  = $protocol."://".$_SERVER['HTTP_HOST'];
+// $base .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+// $config['base_url'] = $base;
 
+
+$config['base_url'] = "https://tml.snsitltd.co.uk/";
 /*
 |--------------------------------------------------------------------------
 | Index File
