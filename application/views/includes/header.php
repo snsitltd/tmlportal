1d0b6bef8f38327ab28006551e0a231d7ba04d82
Devam Joshi <developer@snsitltd.com>
2025-03-10 19:26:23 +0000

    Change Version

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -127,7 +127,7 @@
 			<ul class="nav navbar-nav">
 			<li class="dropdown messages-menu">
 					<a title="Version 1.0.0">
-					  <span> <strong> VL.25.03.1.9[P] </strong></span> 
+					  <span> <strong> VL.25.03.2.15[P] </strong></span> 
 					</a> 
 				</li>  
 			</ul>
