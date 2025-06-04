397670211f35ad1e5adfedd06bbd0d431907c9d6
riddhi thanki <riddhithanki16@gmail.com>
2025-06-04 10:15:12 +0530

    Fixed Issue Of Pdf In Driver Reports

--- application/views/Booking/DriverLoads.php
+++ application/views/Booking/DriverLoads.php
@@ -162,7 +162,7 @@
                                     $searchIDTic = urlencode($ticketNumber);
                                         // Construct the first URL for /All-Tickets with search ID
                                     //$url1 = "https://tml.snsitltd.com/assets/pdf_file/" . $record->TicketPdfName;
-                                      $url1 = "https://tml.snsitltd.com/assets/pdf_file/" . $record->ReceiptName;
+                                      $url1 = "https://tml.snsitltd.com/assets/conveyance/" . $record->ReceiptName;
                                     if ($tipTicketID) {
                                         // Base URL for tip tickets
                                         $baseUrl = "https://tml.snsitltd.com/";
