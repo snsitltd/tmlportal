5f46759ed3b8551c0f9af9694ec3c597e718d40f
riddhi thanki <riddhithanki16@gmail.com>
2025-06-18 10:18:03 +0530

    solve pdf issue in driver reports

--- application/models/Booking_model.php
+++ application/models/Booking_model.php
@@ -3200,6 +3200,7 @@ class Booking_model extends CI_Model{
 		$this->db->select(' tbl_booking_request.OpportunityID ');		 
 		$this->db->select(' tbl_booking_request.OpportunityName ');	 
 		$this->db->select(' tbl_booking_loads1.ReceiptName');
+		$this->db->select(' tbl_tickets.pdf_name'); 
 
 		 
 		$this->db->select(' tbl_tipaddress.TipName ');	  
