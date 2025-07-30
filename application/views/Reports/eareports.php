dafcb72afdf3a77c21cd0c2abeada1bedb3fdd6c
riddhi thanki <riddhi.snsitltd@gmail.com>
2025-07-30 19:05:36 +0530

    Update Design Layout

--- application/views/Reports/eareports.php
+++ application/views/Reports/eareports.php
@@ -21,89 +21,72 @@
                     </div><!-- /.box-header -->
                     <!-- form start -->
                     
-                    <form role="form" action="<?php echo current_url() ?>" method="post" id="tickeReport">
-                        <div class="box-body">
-                            <div class="row d-flex flex-wrap align-items-end" style="gap: 10px;">
-
-                                <!-- Ticket Type -->
-                                <div class="col-md-2">
-                                    <div class="form-group">
-                                        <label>Ticket Type</label>
-                                        <select class="form-control" name="tickettype" id="tickettype">
-                                            <option value="">ALL Types</option>
-                                            <option value="In" <?php if(set_value('tickettype') =="In"){ echo "selected"; } ?>>IN</option>
-                                            <option value="Out" <?php if(set_value('tickettype') =="Out"){ echo "selected"; } ?>>OUT</option>
-                                        </select>
-                                    </div>
-                                </div>
-
-                                <!-- Material -->
-                                <div class="col-md-2">
-                                    <div class="form-group">
-                                        <label>Material</label>
-                                        <select class="form-control" name="material[]" id="material" multiple>
-                                            <?php foreach($materialsRecords as $value){ ?>
-                                                <option value="<?php echo trim($value->MaterialID); ?>" <?php if(isset($_POST['material']) && in_array($value->MaterialID, $_POST['material'])){ echo "selected"; } ?>>
-                                                    <?php echo $value->MaterialName ?>
-                                                </option>
-                                            <?php } ?>
-                                        </select>
-                                    </div>
-                                </div>
-
-                                <!-- County -->
-                                <div class="col-md-2">
-                                    <div class="form-group">
-                                        <label>County</label>
-                                        <select class="form-control" name="county[]" id="county" multiple>
-                                            <?php foreach($county as $value){ ?>
-                                                <option value="<?php echo trim($value->County); ?>" <?php if(isset($_POST['county']) && in_array($value->County, $_POST['county'])){ echo "selected"; } ?>>
-                                                    <?php echo $value->County ?>
-                                                </option>
-                                            <?php } ?>
-                                        </select>
-                                    </div>
-                                </div>
-
-                                <!-- Date Range -->
-                                <div class="col-md-3">
-                                    <div class="form-group">
-                                        <label>Date range:</label>
-                                        <div class="input-group">
-                                            <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
-                                            <input type="text" class="form-control" autocomplete="off" id="reservation" required name="searchdate" value="<?php echo (set_value('searchdate')!=''?set_value('searchdate') : date('d/m/Y').'-'.date('d/m/Y'));?>">
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <!-- Users -->
-                                <div class="col-md-2">
-                                    <div class="form-group">
-                                        <label>Users</label>
-                                        <select class="form-control" name="user[]" id="user" multiple>
-                                            <?php foreach($Users as $value){ ?>
-                                                <option value="<?php echo $value->userId; ?>" <?php if(set_value('user') && in_array($value->userId, set_value('user'))){ echo "selected"; } ?>>
-                                                    <?php echo $value->name ?>
-                                                </option>
-                                            <?php } ?>
-                                        </select>
-                                    </div>
-                                </div>
-
-                    
-                                <?php if(!empty($ticketsRecords)){ ?>
-                                <div class="col-md-3">
-                                    <div class="form-group d-flex" style="gap: 5px; margin-top: 25px;">
-                                        <input type="submit" class="btn btn-primary" value="Search">
-                                        <input type="submit" class="btn btn-primary" value="Export XLS" name="export" />
-                                        <input type="submit" class="btn btn-primary" value="Export PDF" name="export1" />
-                                    </div>
-                                </div>
-                                <?php } ?>
-
-                            </div>
+                    <form role="form" action="<?php echo current_url() ?>" method="post" id="tickeReport" >
+    <div class="box-body">
+        <div class="row">
+            <div class="col-md-2    ">                                
+                <div class="form-group">
+                    <label for="role">Ticket Type</label>
+                    <select class="form-control" name="tickettype" id="tickettype" data-live-search="true" >
+                        <option value="">ALL Types</option> 
+                        <option value="In" <?php if(set_value('tickettype') =="In"){ ?> selected <?php } ?> >IN</option> 
+                        <option value="Out"  <?php if(set_value('tickettype') =="Out"){ ?> selected <?php } ?> >OUT</option> 
+                    </select> 
+                </div> 
+            </div> 
+            <div class="col-md-3">                                
+                <div class="form-group">
+                    <label for="role">Material</label>
+                    <select class="form-control" name="material[]" id="material" multiple data-live-search="true"> 
+                        <?php foreach($materialsRecords as $value){ ?>
+                            <option value="<?php echo trim($value->MaterialID); ?>" <?php if(isset($_POST['material'])) { if(in_array($value->MaterialID, $_POST['material'])){ echo "Selected"; }} ?> ><?php echo $value->MaterialName ?></option>
+                        <?php } ?>
+                    </select> 
+                </div> 
+            </div> 
+            <div class="col-md-2">     
+                <div class="form-group">
+                    <label for="role">County</label>
+                    <select class="form-control" name="county[]" id="county" multiple data-live-search="true"> 
+                        <?php foreach($county as $value){ ?>
+                            <option value="<?php echo trim($value->County); ?>" <?php if(isset($_POST['county'])) { if(in_array($value->County, $_POST['county'])){ echo "Selected"; }} ?> >
+                                <?php echo $value->County ?>
+                            </option>
+                        <?php } ?>
+                    </select> 
+                </div> 
+            </div> 
+            <div class="col-md-3">                                
+                <div class="form-group">
+                    <label for="role">Users</label>
+                    <select class="form-control" name="user[]" id="user" multiple data-live-search="true"> 
+                        <?php foreach($Users as $value){ ?>
+                            <option value="<?php echo $value->userId; ?>" <?php if(set_value('user')){ if (in_array($value->userId, set_value('user'))){ echo "selected"; }} ?> ><?php echo $value->name ?></option>
+                        <?php } ?>
+                    </select>
+                </div> 
+            </div>
+            <div class="col-md-2">     
+                <div class="form-group">
+                    <label>Date range:</label> 
+                    <div class="input-group">
+                        <div class="input-group-addon">
+                            <i class="fa fa-calendar"></i>
                         </div>
-                    </form>
+                        <input type="text" class="form-control pull-right" autocomplete="off" id="reservation" required name="searchdate" value="<?php echo (set_value('searchdate')!=''?set_value('searchdate') : date('d/m/Y').'-'.date('d/m/Y'));?>">
+                    </div> 
+                </div>                            
+            </div>
+        </div>
+    </div> 
+    <div class="box-footer">
+        <input type="submit" class="btn btn-primary" value="Search" />
+        <?php if(!empty($ticketsRecords)){ ?>
+            <input type="submit" class="btn btn-primary" value="Export XLS" name="export" />
+            <input type="submit" class="btn btn-primary" value="Export PDF" name="export1" />
+        <?php } ?>
+    </div>
+</form>
 
                 </div>
             </div>
