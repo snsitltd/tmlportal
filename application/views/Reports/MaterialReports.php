936a50ac2f3ce5eab411599e3adb2e9ac26673b9
riddhi thanki <riddhi.snsitltd@gmail.com>
2025-07-30 12:32:35 +0530

    fix view for user selection in EA-Report

--- application/views/Reports/MaterialReports.php
+++ application/views/Reports/MaterialReports.php
@@ -45,16 +45,7 @@
                                 </div>
 							</div>
 							<div class="row">		
-								<div class="col-md-3">                                
-                                    <div class="form-group">
-                                       <label for="role">Users</label>
-                                       <select class="form-control" name="user[]" id="user" multiple data-live-search="true" > 
-                                            <?php foreach($Users as $value){ ?>
-                                             <option value="<?php echo $value->userId; ?>"  <?php if(set_value('user')){ if (in_array($value->userId, set_value('user'))){ echo "selected"; }} ?> ><?php echo $value->name ?></option>
-                                            <?php } ?>
-                                        </select>
-                                    </div> 
-                                </div>		
+
 								<div class="col-md-4">     
 									<div class="form-group">
 										<label>Date range:</label> 
@@ -67,6 +58,18 @@
 									</div>                            
                                 </div>
                             </div>  
+                            <div class="row"></div>
+                                <div class="col-md-3">                                
+                                    <div class="form-group">
+                                       <label for="role">Users</label>
+                                       <select class="form-control" name="user[]" id="user" multiple data-live-search="true" > 
+                                            <?php foreach($Users as $value){ ?>
+                                             <option value="<?php echo $value->userId; ?>"  <?php if(set_value('user')){ if (in_array($value->userId, set_value('user'))){ echo "selected"; }} ?> ><?php echo $value->name ?></option>
+                                            <?php } ?>
+                                        </select>
+                                    </div> 
+                                </div>
+                            </div>	
                             <div class="row">
                                 <div class="col-md-6"> </div> 
                             </div>     
